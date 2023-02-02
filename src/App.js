a2bc8db4b0339ec015c6fe901820183b40404dc5
raul araque <luis.raul.araque2@gmail.com>
2023-02-02 00:09:27 -0500

    Simple Counter with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+const App = (props) => {
+  return <div>
+    <div className="container p-4">
+      <div className="row bg-black text-light d-flex fs-1">
+        <div class="col card text-bg-secondary m-3 text-center align-self-center">
+          <div class="card-body">
+            <p class="card-text"><FontAwesomeIcon icon={faClock} size="2x" /></p>
+          </div>
+        </div>
+        <div class="col card text-bg-secondary m-3 text-center align-self-center">
+          <div class="card-body">
+            <p class="card-text">0</p>
+          </div>
+        </div>
+        <div class="col card text-bg-secondary m-3 text-center align-self-center">
+          <div class="card-body">
+            <p class="card-text">0</p>
+          </div>
+        </div>
+        <div class="col card text-bg-secondary m-3 text-center align-self-center">
+          <div class="card-body">
+            <p class="card-text">{props.numberFour % 10}</p>
+          </div>
+        </div>
+        <div class="col card text-bg-secondary m-3 text-center align-self-center">
+          <div class="card-body">
+            <p class="card-text">{props.numberThree % 10}</p>
+          </div>
+        </div>
+        <div class="col card text-bg-secondary m-3 text-center align-self-center">
+          <div class="card-body">
+            <p class="card-text">{props.numberTwo % 10}</p>
+          </div>
+        </div>
+        <div class="col card text-bg-secondary m-3 text-center align-self-center">
+          <div class="card-body">
+            <p class="card-text">{props.numberOne % 10}</p>
+          </div>
+        </div>
+
+      </div>
     </div>
-  );
-}
-
+  </div>
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+let time = 0;
+setInterval(()=>{
+  time ++;
+
+ const Four = Math.floor (time/100000);
+  const Three = Math.floor(time/10000);
+  const Two = Math.floor(time/1000);
+  const One = Math.floor(time/100); 
+  root.render(
   <React.StrictMode>
-    <App />
+    <App numberOne ={One} numberTwo={Two} numberThree = {Three} numberFour = {Four}/>
   </React.StrictMode>
 );
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
